c7f807f5e9a7c9f61908612c83344193fda991bb
Ricardo <Ricardoduinkerken@live.nl>
2023-04-16 19:52:09 +0200

    add bow and arrow

---
+++ Assets/_ThrongsOfChaos/Scripts/BowAndArrow/Bow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Bow : XRGrabInteractable
+{
+    // Empty
+    [SerializeField] private MeshCollider meshCollider;
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        meshCollider.isTrigger = true;
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        meshCollider.isTrigger = false;
+    }
+}
